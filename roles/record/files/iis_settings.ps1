70a7b3cf7e54db69ee222ebfbcdf0b8d06463d67
Joe Graham <jmg3484@rit.edu>
2019-02-20 03:34:37 -0500

    Finish implementing Ansible

--- roles/record/files/iis_settings.ps1
+++ roles/record/files/iis_settings.ps1
@@ -1,3 +1,7 @@
 $ConfigSection = Get-IISConfigSection -SectionPath "system.webServer/fastCgi"
 $cgiCollection = Get-IISConfigCollection -ConfigElement $ConfigSection
 New-IISConfigCollectionElement -ConfigCollection $cgiCollection -ConfigAttribute @{"fullPath" = "C:\PHP\php-cgi.exe"}
+$PoolSection = Get-IISConfigSection -SectionPath "system.applicationHost/applicationPools"
+$Child = Get-IISConfigElement -ConfigElement $PoolSection -ChildElementName "applicationPoolDefaults"
+$processModel = Get-IISConfigElement -ConfigElement $Child -ChildElementName "processModel"
+Set-IISConfigAttributeValue -ConfigElement $processModel -AttributeName "identityType" -AttributeValue "LocalSystem"
